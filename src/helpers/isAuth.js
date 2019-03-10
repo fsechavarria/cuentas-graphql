8f12862903362041daa99b1239e036f4a1e90477
fsechavarria <fsechavarria@gmail.com>
2019-03-10 19:47:43 -0300

    Fixed jwt header check

--- src/helpers/isAuth.js
+++ src/helpers/isAuth.js
@@ -2,12 +2,16 @@
 import jwt from 'jsonwebtoken'
 
 export const authenticate = ({ headers }) => {
-  const Authorization = headers.Authorization
+  const Authorization = headers.Authorization || headers.authorization
 
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
-    const { userId } = jwt.verify(token, process.env.SECRET)
-    return userId
+    try {
+      const { userId } = jwt.verify(token, process.env.SECRET)
+      return userId
+    } catch (err) {
+      throw new Error('Unauthorized')
+    }
   }
 
   throw new Error('Unauthorized')
